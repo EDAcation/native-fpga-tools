3c8c24cd8a61db76eb333996802708c7ded5ae8b
Mike A. <git@mikealmel.ooo>
2024-07-04 14:42:16 +0200

    Build custom yosys target

---
+++ rules/targets.py
@@ -0,0 +1,17 @@
+"""
+Contains custom OSS CAD Suite targets that should be built.
+
+Targets called <toolname>-full are functionally equivalent to the existing <toolname> target,
+but are built as a top_package. This means that it will contain the libraries necessary
+to run it as a standalone tool.
+
+This module gets called with cwd = <gitroot>/oss-cad-suite-build, so we can import from there.
+"""
+
+from src.base import Target
+
+Target(
+	name='yosys-full',
+	dependencies=[ 'yosys' ],
+    top_package=True
+)
